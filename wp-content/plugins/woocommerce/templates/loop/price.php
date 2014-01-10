4fd4fca495b5b60c074de8af84e54f0d6fc03d84
RoyEric <eric@alliedscientificpro.com>
2014-01-10 13:40:33 -0500

    Add paypal image to price tag

--- wp-content/plugins/woocommerce/templates/loop/price.php
+++ wp-content/plugins/woocommerce/templates/loop/price.php
@@ -13,5 +13,8 @@ global $product;
 ?>
 
 <?php if ( $price_html = $product->get_price_html() ) : ?>
-	<span class="price"><?php echo $price_html; ?></span>
+	<span class="price">
+		<?php echo $price_html; ?>
+		<img style="float:right" src="http://alliedscientificpro.com/wp-content/plugins/ASP_plugin/images/paypal.png" alt="Test Payment method">
+	</span>
 <?php endif; ?>
