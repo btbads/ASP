fd0907042c8ee54b672b7b0a6b47403400774f08
RoyEric <eric@alliedscientificpro.com>
2014-01-10 14:00:28 -0500

    Remove paypal image tag

--- wp-content/plugins/woocommerce/templates/loop/price.php
+++ wp-content/plugins/woocommerce/templates/loop/price.php
@@ -15,6 +15,5 @@ global $product;
 <?php if ( $price_html = $product->get_price_html() ) : ?>
 	<span class="price">
 		<?php echo $price_html; ?>
-		<img style="float:right" src="http://alliedscientificpro.com/wp-content/plugins/ASP_plugin/images/paypal.png" alt="Test Payment method">
 	</span>
 <?php endif; ?>
